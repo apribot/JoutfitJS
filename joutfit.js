0f735d92f1c0c6b87eef0f4d4968f19bac430ba2
Justin Mikula <justin.mikula@realpage.com>
2017-03-30 17:26:41 -0400

    updated to vanilla js

--- joutfit.js
+++ joutfit.js
@@ -8,40 +8,58 @@
 */
 
 var joutfit = {};
+
 joutfit.get = function(jacket, callback) {
+    if(!joutfit.controllerURL) {
+        console.log('No controllerURL set :(');
+        return;
+    }
 
-  if(!joutfit.controllerURL) {
-    console.log('No controllerURL set :(');
-    return;
-  }
+    var obj = {jean:jacket};
+    var params = this.serialize(obj);
 
-  $.ajax({
-      type: "POST",
-      url: joutfit.controllerURL, 
-      data: { jean:jacket }
-  }).done(function( msg ) {
-      if(joutfit.IsJsonString(msg)) 
-      {
-        var job = JSON.parse(msg);
-
-        for(key in job) {
-            $('[joutfit="' + key + '"]').html(job[key]);
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", joutfit.controllerURL);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            if(joutfit.IsJsonString(xhr.responseText)) {
+                var job = JSON.parse(xhr.responseText);
+                for(key in job) {
+                        document.querySelector('[joutfit="' + key + '"]').innerHTML = job[key];
+                }
+            }
+            if (typeof callback == 'function') { 
+                callback();
+            }
         }
-        
-      }
-      if (typeof callback == 'function') { 
-        callback();
-      }
-  });
+        else if (xhr.status !== 200) {
+                console.log('Joutfit request failed.    Returned status of ' + xhr.status);
+        }
+    };
+    xhr.send(params);
 };
 
 joutfit.controllerURL = null;
 
 joutfit.IsJsonString = function(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
+        try {
+                JSON.parse(str);
+        } catch (e) {
+                return false;
+        }
+        return true;
+}
+
+joutfit.serialize = function(obj, prefix) {
+  var str = [], p;
+  for(p in obj) {
+    if (obj.hasOwnProperty(p)) {
+      var k = prefix ? prefix + "[" + p + "]" : p, v = obj[p];
+      str.push((v !== null && typeof v === "object") ?
+        this.serialize(v, k) :
+        encodeURIComponent(k) + "=" + encodeURIComponent(v));
     }
-    return true;
+  }
+  return str.join("&");
 }
